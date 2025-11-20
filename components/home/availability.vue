5eb4d495628d3b73016d6fed5ccf17ac9bcb9ca9
TonyBimenyi <tonyblaisb@gmail.com>
2025-11-21 02:38:55 +0800

    asaa

---
+++ components/home/availability.vue
@@ -0,0 +1,90 @@
+<template>
+  <section class="features-section">
+    <h2 class="section-title">Check Availability</h2>
+
+    <div class="features-flex">
+      <div class="feature-card fade-up" ref="cards">
+        <p class="feature-text">
+          Find the perfect date for your event. Our booking calendar shows real-time availability.
+        </p>
+
+        <button>View Available Dates</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script>
+
+
+export default {
+  mounted() {
+    const { animateOnScroll } = useScrollAnimation()
+
+    // Your composable expects an array of elements
+    animateOnScroll([this.$refs.cards])
+  }
+}
+</script>
+
+
+<style scoped>
+.features-section {
+  padding: 60px 20px;
+  text-align: center;
+  background-color: rgba(242, 242, 242, 0.638);
+}
+
+.section-title {
+  font-size: 2.5rem;
+  margin-bottom: 40px;
+  color: #0a1f44;
+}
+.feature-card button{
+    background: var(--primary-color);
+    color: white;
+    border: none;
+    padding: 15px 20px;
+    font-size: var(--fs-base);
+    border-radius: 8px;
+    margin-top: 30px;
+    cursor: pointer;
+    
+}
+.feature-text{
+    color: #6b7a90;
+    font-size: var(--fs-base);
+}
+.feature-card button:hover{
+    background: transparent;
+    border: 3px solid var(--primary-color);
+    transition: .2s ease-in;
+    color: var(--primary-color);
+}
+
+/* MAKE THE CARD VISIBLE */
+/* FADE IN UP ANIMATION */
+.fade-up {
+  opacity: 0;
+  transform: translateY(35px);
+  transition: opacity 0.8s ease, transform 0.8s ease;
+}
+
+.fade-up.visible {
+  opacity: 1;
+  transform: translateY(0);
+  animation: fadeInUp 1.2s ease forwards;
+}
+
+@keyframes fadeInUp {
+  0% {
+    opacity: 0;
+    transform: translateY(35px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
