bc11454821bc1e5900936973d5732e06f238f357
TonyBimenyi <tonyblaisb@gmail.com>
2025-11-29 22:54:10 +0800

    packages done

---
+++ pages/services.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="about_cont">
+        <ReusablePageHeader/>
+        <ServicesPackages/>
+    </div>
+</template>
+<style scopped>
+    .about_cont{
+        margin-top: 90px;
+        /* margin-bottom: 90px; */
+    }
+</style>
