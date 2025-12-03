66ef5dbc44714db40f81170bd4c831b2d0a39484
TonyBimenyi <tonyblaisb@gmail.com>
2025-12-03 15:50:46 +0800

    creating contact

---
+++ pages/contact.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="contact_cont">
+        <ReusablePageHeader/>
+    </div>
+</template>
+<style scopped>
+    .contact_cont{
+        margin-top: 90px;
+        /* margin-bottom: 90px; */
+    }
+</style>
