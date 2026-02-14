83a0bce185f3ab9ae37fc4a425c1c1c7402f1ea3
TonyBimenyi <tonyblaisb@gmail.com>
2026-02-14 11:06:25 +0800

    adding pages

--- pages/admin/payments.vue
+++ pages/admin/payments.vue
@@ -164,7 +164,7 @@ export default {
    ──────────────────────────────────────────────── */
 
 .payments-page {
-  padding: 24px;
+  /* padding: 24px; */
   background: #f8fafc;
   min-height: 100vh;
 }
